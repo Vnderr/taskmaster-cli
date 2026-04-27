62d3698587fef0007612c46be1bcfaf0246379e9
unknown <SSDD@POAPCA02LC0512.duoc.acad>
2026-04-27 11:26:09 -0400

    chore: initial project structure

---
+++ task.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const PATH = './data/task.json';
+
+function readTask(){
+    return JSON.parse(fs.readFileSync(PATH, 'utf8'));
+}
+
+function writeTask(task) {
+    fs.writeFileSync(PATH, JSON.stringify(task, null, 2));
+}
+
+module.exports = { readTask, writeTask};
